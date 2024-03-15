69ab18ebcf823c7e7de47f2c5af923fb985022de
Shahadat Siddikee Shawon <ssiddikee111@gmail.com>
2024-03-15 23:03:13 +0600

    first commit

---
+++ project_1/project_1/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse 
+
+def contact(request):
+    return HttpResponse(" this is contact page")
+
+
+def home (request):
+    return HttpResponse("this is home page")
+
+
